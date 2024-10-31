904e32ec9afe6430b33eb5e173977de0fd56588a
FarmanKhan2242 <farman03170218621@gmail.com>
2024-10-31 17:03:16 +0500

    small update

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ let main = document.querySelector('main')                   //todo-items-box
 
 btnAdd.addEventListener('click',()=>{
     //Adding items in list 
-    let input = document.querySelector('#todo-input'); 
-    if(input.value.length > 0){
-        main.innerHTML += `<div class='div-list'>
-            <h4 class='list-item'>${input.value}</h4> <button id='remove-btn' class='remove-btns'>Remove</button> </div>`
-        input.value = '';
+    let input = document.querySelector('#todo-input');       //input item
+    //creating div for pushing to list
+    let div = document.createElement('div')
+    div.className = 'div-list';
+    //creating h4 to add in div
+    let h4 = document.createElement('h4')
+    h4.className = 'list-item';
+    h4.appendChild(document.createTextNode(input.value));
+    //creating button to push in div
+    let button = document.createElement('button')
+    button.appendChild(document.createTextNode('Remove'))
+    button.className = 'remove-btns'
+
+    div.append(h4,button)         //pushing button and h4 in div
+    if(input.value.length > 0){         // if input text is grater than 1
+        main.append(div)               // so push the div into list
+        input.value = '';             // & do the input null  
     }
 
 
